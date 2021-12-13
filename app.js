76ccfa1adbcb1a9a022b8e381d9ac981b665178e
hermkan <hermann.kanga.dev@gmail.com>
2021-12-12 22:36:30 -0500

    added base css and static file serving

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ const app = express();
 app.use(authRoutes);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 app.listen('3002');
