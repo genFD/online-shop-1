f83341c7a11aa74d951db137b20222e54ea89102
hermkan <hermann.kanga.dev@gmail.com>
2021-12-12 21:54:31 -0500

    added ejs and & first views

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ const express = require('express');
 const authRoutes = require('./routes/auth.routes');
 const app = express();
 app.use(authRoutes);
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.listen('3002');
