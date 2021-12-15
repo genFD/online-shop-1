535d86b10c757f51e27bec027ad2f09282a8ba9b
hermkan <hermann.kanga.dev@gmail.com>
2021-12-15 13:30:42 -0500

    implemented authentication and user login

--- models/user.model.js
+++ models/user.model.js
@@ -13,7 +13,9 @@ class User {
       city: city,
     };
   }
-
+  getUserWithSameEmail() {
+    return db.getDb().collection('users').findOne({ email: this.email });
+  }
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
 
@@ -23,6 +25,9 @@ class User {
       name: this.name,
       address: this.address,
     });
+  }
+  hasMatchingPassword(hashedPassword) {
+    return bcrypt.compare(this.password, hashedPassword);
   }
 }
 
